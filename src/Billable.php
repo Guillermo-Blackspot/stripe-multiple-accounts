2e7e4aeb1a48ea7079608185261bafe5a4d1860b
Guillermo-Rodriguez <guillermo.blackspot@gmail.com>
2023-02-01 20:31:48 -0600

    Se agrego metodo para limipiar cache de memoria

--- src/Billable.php
+++ src/Billable.php
@@ -6,11 +6,21 @@ use BlackSpot\StripeMultipleAccounts\Concerns\HandlesServiceIntegrations;
 use BlackSpot\StripeMultipleAccounts\Concerns\ManagesAuthCredentials;
 use BlackSpot\StripeMultipleAccounts\Concerns\ManagesCustomerInformation;
 use BlackSpot\StripeMultipleAccounts\Concerns\ManagesPaymentMethods;
+use BlackSpot\StripeMultipleAccounts\Relationships\HasServiceIntegrationUsers;
 
 trait Billable
 {
+    use HasServiceIntegrationUsers;
     use HandlesServiceIntegrations;
     use ManagesAuthCredentials;
     use ManagesCustomerInformation;
     use ManagesPaymentMethods;
+
+
+    public function clearStripeBillableCache()
+    {
+        $this->stripeServiceIntegrationRecentlyFetched = null;
+        $this->stripeCustomerRecentlyFetched           = null;
+        $this->stripeCustomerIdRecentlyFetched         = null;
+    }
 }
