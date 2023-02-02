930a5289de17653052c0db70c4a97d7bfabf7148
Guillermo-Rodriguez <guillermo.blackspot@gmail.com>
2023-02-02 14:44:45 -0600

    Se agrego trait al Billable para habilitar suscripciones

--- src/Billable.php
+++ src/Billable.php
@@ -6,11 +6,13 @@ use BlackSpot\StripeMultipleAccounts\Concerns\HandlesServiceIntegrations;
 use BlackSpot\StripeMultipleAccounts\Concerns\ManagesAuthCredentials;
 use BlackSpot\StripeMultipleAccounts\Concerns\ManagesCustomerInformation;
 use BlackSpot\StripeMultipleAccounts\Concerns\ManagesPaymentMethods;
+use BlackSpot\StripeMultipleAccounts\Relationships\HasServiceIntegrationSubscriptions;
 use BlackSpot\StripeMultipleAccounts\Relationships\HasServiceIntegrationUsers;
 
 trait Billable
 {
     use HasServiceIntegrationUsers;
+    use HasServiceIntegrationSubscriptions;
     use HandlesServiceIntegrations;
     use ManagesAuthCredentials;
     use ManagesCustomerInformation;
