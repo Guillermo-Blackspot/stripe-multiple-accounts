c283096d5b18e2148af395bfd8d2a8860236abec
Guillermo-Rodriguez <guillermo.blackspot@gmail.com>
2023-02-17 17:40:51 -0600

    Correccion

--- src/Models/ServiceIntegrationSubscription.php
+++ src/Models/ServiceIntegrationSubscription.php
@@ -9,7 +9,7 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use DateTimeInterface;
-use Stripe\Subscrition as StripeSubscription;
+use Stripe\Subscription as StripeSubscription;
 
 class ServiceIntegrationSubscription extends Model
 {
